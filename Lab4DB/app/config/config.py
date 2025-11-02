237cea076ed951fcf9c7ac84ffd00a5c480d214a
Vilen <150962085+VilenSolovey@users.noreply.github.com>
2025-11-02 15:37:22 +0200

    Fix database URI scheme in config.py

--- Lab4DB/app/config/config.py
+++ Lab4DB/app/config/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"mysql+mysqlconnector://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
         f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
